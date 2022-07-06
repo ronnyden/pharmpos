1bbc7bb4f34eb7ef70f3bf073f4a38db02f6caf1
ronnyden <dennisor98@gmail.com>
2022-07-06 14:51:42 +0300

    user routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/user/register',[UserController::class,'register']);
+Route::post('/user/login',[UserController::class,'login']);
+Route::put('/user/update',[UserController::class,'updateUser']);
+Route::delete('/user/delete',[UserController::class,'deleteUser']);
